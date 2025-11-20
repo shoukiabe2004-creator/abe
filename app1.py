8a016560ee082ffcc8643daad5d39cb1e4242b95
安部 <shoukiabe2004@gmail.com>
2025-11-20 16:21:50 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title("こんにちは、吉村ゼミ")
+
+name = st.text_input("好きな言葉を入力してください")
+
+st.write(name)
+
+camera = st.camera_input("写真を撮影します！")
+if camera
+    st.image(camera,caption="写真",use_column_width=True)
+
+
