b6609e25bd2f663409d4e8307c27ef8625765c0b
安部 <shoukiabe2004@gmail.com>
2025-11-27 15:23:50 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,6 +8,8 @@ st.write(name)
 
 st.checkbox("同意します")
 
+st.selectbox("次の中から現住所を教えて"["京都府","大阪府"])
+
 camera = st.camera_input("写真を撮影します！")
 if camera:
     st.image(camera,caption="写真",use_column_width=True)
