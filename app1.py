d4829b1a596e52a3de719512f63d1eee95fcae2d
安部 <shoukiabe2004@gmail.com>
2025-11-27 15:45:50 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -19,6 +19,12 @@ st.write(score)
 
 st.radio("性別を選択してください",["男性","女性"])
 
+list=[
+  {"latitude":35.05,"longitude":135.76},
+  {"latitude":35.04,"longitude":135.75},
+] 
+st.map(list)
+
 camera = st.camera_input("写真を撮影します！")
 if camera:
     st.image(camera,caption="写真",use_column_width=True)
